f1f7fd44626564d73539f4a2c543e60bd810a334
Wei <wei_x@apple.com>
2019-06-06 23:13:48 -0400

    lite is working

--- app.py
+++ app.py
@@ -6,16 +6,19 @@ from flask import request
 app = Flask(__name__)
 from ml_manager import MLManager
 
+app = Flask(__name__)
+app.debug = True
+
 mlmanager = MLManager('lite')
 
 @app.route('/postjson', methods=['POST'])
 def post():
     print(request.is_json)
     content = request.get_json()
-    #print(content)
+    # print(content)
     sen1 = content['sen1']
     sen2 = content['sen2']
 
-    print(mlmanager.get_STS_score(sen1, sen2))
-    return 'JSON posted'
+    col = mlmanager.get_STS_score_lite(sen1, sen2)
+    return f'{col}'
 app.run(host='0.0.0.0', port=5000)
--- ml_manager.py
+++ ml_manager.py
@@ -12,9 +12,23 @@ import datetime
 
 class MLManager:
     def __init__(self, model_type):
-        self.model_type = model_type
-        self.module = self.create_tf_module(model_type)
-        self.session, self.sp = self.create_tf_session(model_type, self.module)
+        print("model started")
+        # self.model_type = model_type
+        # # self.module = self.create_tf_module(model_type)
+        # # self.session, self.sp = self.create_tf_session(model_type, self.module)
+        #
+        # module = hub.Module("https://tfhub.dev/google/universal-sentence-encoder-lite/2")
+        # self.session = tf.Session()
+        # spm_path = self.session.run(module(signature="spm_path"))
+        # self.sp = spm.SentencePieceProcessor()
+        # self.sp.Load(spm_path)
+        # self.session.run(tf.global_variables_initializer())
+        # self.session.run(tf.tables_initializer())
+
+        #large
+        # self.session = tf.Session()
+        # self.session.run(tf.global_variables_initializer())
+        # self.session.run(tf.tables_initializer())
 
     def create_tf_session(self, model_type, module):
         return self.create_tf_session_lite(module)
@@ -32,7 +46,7 @@ class MLManager:
         session.run(tf.tables_initializer())
         return session, sp
 
-    def process_to_IDs_in_sparse_format(sp, sentences):
+    def process_to_IDs_in_sparse_format(self, sp, sentences):
         # An utility method that processes sentences with the sentence piece processor
         # 'sp' and returns the results in tf.SparseTensor-similar format:
          # (values, indices, dense_shape)
@@ -43,14 +57,69 @@ class MLManager:
         indices = [[row, col] for row in range(len(ids)) for col in range(len(ids[row]))]
         return (values, indices, dense_shape)
 
-    def get_STS_score(self, sentence1, sentence2):
+    def get_STS_score_lite(self, sentence1, sentence2):
+        module = hub.Module("https://tfhub.dev/google/universal-sentence-encoder-lite/2")
         input_placeholder = tf.sparse_placeholder(tf.int64, shape=[None, None])
-        encodings = self.module(inputs=dict(values=input_placeholder.values,indices=input_placeholder.indices,dense_shape=input_placeholder.dense_shape))
-        values, indices, dense_shape = self.process_to_IDs_in_sparse_format(self.sp, [sentence1, sentence2])
-        message_embeddings = self.session.run(
-        encodings,feed_dict={input_placeholder.values: values,
-                   input_placeholder.indices: indices,
-                   input_placeholder.dense_shape: dense_shape})
+        encodings = module(
+            inputs=dict(
+                values=input_placeholder.values,
+                indices=input_placeholder.indices,
+                dense_shape=input_placeholder.dense_shape))
+        with tf.Session() as sess:
+            spm_path = sess.run(module(signature="spm_path"))
+        sp = spm.SentencePieceProcessor()
+        sp.Load(spm_path)
+        print("SentencePiece model loaded at {}.".format(spm_path))
 
-        corr = np.inner(message_embeddings, message_embeddings)
-        return corr
+        with tf.Session() as session:
+            session.run(tf.global_variables_initializer())
+            session.run(tf.tables_initializer())
+            messages = [sentence1, sentence2]
+            values, indices, dense_shape = self.process_to_IDs_in_sparse_format(sp, messages)
+            message_embeddings = session.run(
+                encodings,
+                feed_dict={input_placeholder.values: values,
+                        input_placeholder.indices: indices,
+                        input_placeholder.dense_shape: dense_shape})
+            corr = np.inner(message_embeddings, message_embeddings)
+            return corr.item((0,1))
+
+        # encodings = module(inputs=dict(values=input_placeholder.values,indices=input_placeholder.indices,dense_shape=input_placeholder.dense_shape))
+        # message = [sentence1, sentence2]
+        # values, indices, dense_shape = self.process_to_IDs_in_sparse_format(self.sp, message)
+        # message_embeddings = self.session.run(
+        # encodings,feed_dict={input_placeholder.values: values,
+        #            input_placeholder.indices: indices,
+        #            input_placeholder.dense_shape: dense_shape})
+        #
+        # corr = np.inner(message_embeddings, message_embeddings)
+        # return corr
+
+
+    def get_STS_score_large(self, sentence1, sentence2):
+        with tf.Session() as session:
+
+            session.run(tf.global_variables_initializer())
+            session.run(tf.tables_initializer())
+
+
+            print('inside get sts')
+            module_url = "https://tfhub.dev/google/universal-sentence-encoder/2" #@param ["https://tfhub.dev/google/universal-sentence-encoder/2", "https://tfhub.dev/google/universal-sentence-encoder-large/3"]
+        # Import the Universal Sentence Encoder's TF Hub module
+            embed = hub.Module(module_url)
+            print('inside get sts 1')
+
+            messages = [sentence1, sentence2]
+            similarity_input_placeholder = tf.placeholder(tf.string, shape=(None))
+            similarity_message_encodings = embed(similarity_input_placeholder)
+            message_embeddings_ = session.run(similarity_message_encodings, feed_dict={similarity_input_placeholder: messages})
+            corr = np.inner(message_embeddings_, message_embeddings_)
+
+            print('inside get sts 2')
+
+            return  corr
+
+
+
+# manager = MLManager("large")
+# manager.get_STS_score_lite("wei has an apple", "wei has an orange")
